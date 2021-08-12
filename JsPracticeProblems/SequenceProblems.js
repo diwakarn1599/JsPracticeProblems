1da24257cf2a9ef9356e0108df770008e499177f
Diwakar <diwakar.n@tvsnext.io>
2021-08-12 13:37:50 +0530

    [Diwakar] Add: Sum and average of 5 two digits random numbers

--- JsPracticeProblems/SequenceProblems.js
+++ JsPracticeProblems/SequenceProblems.js
@@ -15,7 +15,24 @@ let count =0,sum=0;
 while(count<2)
 {
     diceRoll = (Math.floor(Math.random() *10)%6)+1;
+    //appending all dicerolls to sum
     sum +=diceRoll;
     count++;
 }
-console.log(`Sum of two dicerolls is ${sum}`);
+//console.log(`Sum of two dicerolls is ${sum}`);
+
+//Uc4
+//intialising variables
+let twoDigNum;
+count=0;
+sum=0;
+while(count<5)
+{
+    twoDigNum = Math.floor(Math.random() *100);
+    console.log(`Number = ${twoDigNum}`);
+    //addinng random digits
+    sum +=twoDigNum;
+    count++;
+}
+console.log(`Sum of 5 two digits number is = ${sum}`);
+console.log(`Average of the sum of the 5 two digits number is = ${sum/5}`);
