1ae51dbd51c56d5beb2997e836eee410057650b1
Diwakar <diwakar.n@tvsnext.io>
2021-08-12 13:16:55 +0530

    [Diwakar]add: genrate random b/w 1-6

--- JsPracticeProblems/SequenceProblems.js
+++ JsPracticeProblems/SequenceProblems.js
@@ -2,4 +2,9 @@
 //Uc1
 //genrate random value b/w 0-9
 let randomValue = Math.floor(Math.random() *10); 
-console.log(randomValue);
+//console.log(randomValue);
+
+//Uc2
+//dice roll b/w 1-6
+let diceRoll = (Math.floor(Math.random() *10)%6)+1;
+console.log(diceRoll);
