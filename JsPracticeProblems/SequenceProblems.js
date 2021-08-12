9821866210ca93bc0a26f1097cc9468ba95ab5b1
Diwakar <diwakar.n@tvsnext.io>
2021-08-12 13:30:26 +0530

    [Diwakar]Add: get two dicerolls and print the sum

--- JsPracticeProblems/SequenceProblems.js
+++ JsPracticeProblems/SequenceProblems.js
@@ -6,5 +6,16 @@ let randomValue = Math.floor(Math.random() *10);
 
 //Uc2
 //dice roll b/w 1-6
-let diceRoll = (Math.floor(Math.random() *10)%6)+1;
-console.log(diceRoll);
+let diceRoll;
+diceRoll = (Math.floor(Math.random() *10)%6)+1;
+//console.log(diceRoll);
+
+//Uc3
+let count =0,sum=0;
+while(count<2)
+{
+    diceRoll = (Math.floor(Math.random() *10)%6)+1;
+    sum +=diceRoll;
+    count++;
+}
+console.log(`Sum of two dicerolls is ${sum}`);
