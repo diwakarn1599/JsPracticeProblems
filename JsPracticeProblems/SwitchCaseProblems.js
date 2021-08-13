e2406b967948e1ddcca98cfe16ca44442092f832
Diwakar <diwakar.n@tvsnext.io>
2021-08-13 16:46:12 +0530

    [Diwakar] Add: Print unit, ten, hunderd using switch case

--- JsPracticeProblems/SwitchCaseProblems.js
+++ JsPracticeProblems/SwitchCaseProblems.js
@@ -41,6 +41,7 @@ switch(num)
 }
 */
 //-------------------------------------------------UC2--------------------------------
+/*
 const num = parseInt(prompt('Enter the number of day in week?'));
 
 switch(num)
@@ -68,5 +69,32 @@ switch(num)
         break;
     default:
         console.log("Enter the day");
+        break;
+}
+*/ 
+//-----------------------------------UC3------------------------------------
+const num = parseInt(prompt('Enter the number?'));
+switch(num)
+{
+    case 1:
+        console.log("One");
+        break;
+    case 10:
+        console.log("Ten");
+        break;
+    case 100:
+        console.log("Hundred");
+        break;
+    case 1000:
+        console.log("Thousand");
+        break;
+    case 10000:
+        console.log("Ten thousand");
+        break;
+    case 100000:
+        console.log("1 Lakh");
+        break;
+    default:
+        console.log("enter correct number");
         break;
 }
