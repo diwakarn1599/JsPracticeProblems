a79aee686eb710c9a5ccc7896b38d20c4f551f24
Diwakar <diwakar.n@tvsnext.io>
2021-08-13 22:53:48 +0530

    [Diwakar]Add: check whether number is prime or not

--- JsPracticeProblems/ForLoopProblems.js
+++ JsPracticeProblems/ForLoopProblems.js
@@ -13,6 +13,7 @@ for(let i=0;i<=n;i++)
 }
 */
 //---------------------------------------------------UC2----------------------------------------------------
+/** 
 let n = parseInt(prompt('Enter the value of n?'));
 if(n!=0)
 {
@@ -27,3 +28,29 @@ else
 {
     console.log('The value should not be zero');
 }
+*/
+//------------------------------------------------UC3---------------------------------------------------------
+let n = parseInt(prompt('Enter the value of n?'));
+
+let checkPrime = (num) =>
+{
+    let flag=0;
+    if(num==2)
+        return true;
+    else if (num % 2 == 0)
+        return false;
+    else
+        for(let i=3;i*i<=num;i++)
+            if(num%i==0)
+            {
+                flag=1;
+                break;
+            }
+        if(flag==1)
+            return false;
+        else
+            return true;
+        
+}
+
+console.log(checkPrime(n)?`${n} is Prime number`:`${n} is not a prime number`);
