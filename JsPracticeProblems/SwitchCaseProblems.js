d1d35785aa369336d68462aacb77b5ca8292bd9e
Diwakar <diwakar.n@tvsnext.io>
2021-08-13 16:59:26 +0530

    [Diwakar]Add: inch to feet, feet to inch, meter converter

--- JsPracticeProblems/SwitchCaseProblems.js
+++ JsPracticeProblems/SwitchCaseProblems.js
@@ -73,6 +73,7 @@ switch(num)
 }
 */ 
 //-----------------------------------UC3------------------------------------
+/** 
 const num = parseInt(prompt('Enter the number?'));
 switch(num)
 {
@@ -96,5 +97,30 @@ switch(num)
         break;
     default:
         console.log("enter correct number");
+        break;
+}
+*/
+//------------------------------------------------------UC4--------------------------------------
+console.log("1.Feet to Inch\n2.Feet to Meter\n3.Inch to Feet\n4.Meter to Feet");
+switch(parseInt(prompt('Enter the Choice?')))
+{
+    case 1:
+        let feet = parseInt(prompt('Enter the feets?'));
+        console.log(` ${feet} feets = ${feet*12} inches`);
+        break;
+    case 2:
+        let feets = parseInt(prompt('Enter the feets?'));
+        console.log(` ${feets} feets = ${feets/3.281} meter`);
+        break;
+    case 3:
+        let inch = parseInt(prompt('Enter the inches?'));
+        console.log(` ${inch} inches = ${inch/12} feets`);
+        break;
+    case 4:
+        let meter = parseInt(prompt('Enter the meter?'));
+        console.log(` ${meter} meters = ${meter*3.281} feets`);
+        break;
+    default:
+        console.log("Enter the correct value");
         break;
 }
