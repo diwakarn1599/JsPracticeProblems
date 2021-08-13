c042dce1384aeb799514fbed27cd27a2f7531083
Diwakar <diwakar.n@tvsnext.io>
2021-08-13 23:46:40 +0530

    [Diwakar] add: FInd the magic number problem

--- JsPracticeProblems/WhileLoopProblems.js
+++ JsPracticeProblems/WhileLoopProblems.js
@@ -2,6 +2,7 @@
 "use strict";
 //initialising the prompt
 const prompt = require('prompt-sync')();
+/**
 //get input from the user
 var n = parseInt( prompt('Enter the power?'));
 let i=0;
@@ -12,4 +13,43 @@ do{
     console.log(`2 ^ ${i} = ${power}`);
     i++;
 } while(i<=n && power<=128)
-//given 128 because do while runs 1 more time 
+//given 128 because do while runs 1 more time
+ */
+//-----------------------------------Uc2--------------------------
+var first = 0,last=100;
+var found = false;
+//function to find magic number
+let checkValue = (mid) =>
+{
+    console.log(`PRESS\n1.If your number is ${mid}\n2.If your number is less than ${mid}\n3.If your number is greater than ${mid}`);
+    switch (parseInt( prompt('Enter the choice?')))
+        {
+            case 1:
+                console.log(`Your number is ${mid}`);
+                found = true;
+                break;
+            case 2:
+                last = mid - 1;
+                break;
+            case 3:
+                first = mid + 1;
+                break;
+        }
+}
+console.log(`Think a number between ${first} - ${last} `);
+//get input from user
+var num = parseInt( prompt('Enter the number?'));
+while (first != last && found == false)
+{
+    //finding the mid value
+    var mid = Math.floor((first + last) / 2);
+    //call the function
+    checkValue(mid);
+}
+if (first == last)
+{
+    //if first == last number is reached
+    console.log(`Your number is ${first}`);
+    
+}
+
