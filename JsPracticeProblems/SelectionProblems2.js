2f409086f14f3660a8e28ba7f24c197947f82122
Diwakar <diwakar.n@tvsnext.io>
2021-08-13 15:36:34 +0530

    [Diwakar]Add: For given number print the corresponding day

--- JsPracticeProblems/SelectionProblems2.js
+++ JsPracticeProblems/SelectionProblems2.js
@@ -1,7 +1,7 @@
 "use strict";
 const prompt = require('prompt-sync')();
 /*---------------------------------------Uc1----------------------------- */
-//
+/*
 const num = parseInt(prompt('Enter the number?'));
 if(num==1)
 {
@@ -46,4 +46,39 @@ else if(num==0)
 else
 {
     console.log("Enter Single digit number");
+}
+*/
+//---------------------------------------------UC2-------------------------------------
+const num = parseInt(prompt('Enter the day in number \nEx- 1.Sunday,2.Monday,...?'));
+if(num==1)
+{
+    console.log("Sunday");
+}
+else if(num==2)
+{
+    console.log("Monday");
+}
+else if(num==3)
+{
+    console.log("Tuesday");
+}
+else if(num==4)
+{
+    console.log("Wednesday");
+}
+else if(num==5)
+{
+    console.log("Thursday");
+}
+else if(num==6)
+{
+    console.log("Friday");
+}
+else if(num==7)
+{
+    console.log("Saturday");
+}
+else
+{
+    console.log("Enter the correct number");
 }
