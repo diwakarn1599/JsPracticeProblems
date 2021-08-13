3ca89457feb32349c3727b743b4933d63ef74078
Diwakar <diwakar.n@tvsnext.io>
2021-08-13 12:40:05 +0530

    [Diwakar] Check date and month if it is between range

--- JsPracticeProblems/SelectionProblems.js
+++ JsPracticeProblems/SelectionProblems.js
@@ -1,5 +1,7 @@
 "use strict";
+const prompt = require('prompt-sync')();
 //function to genrate 3 digit number
+/*
 function getThreeDigitRandomNumber()
 {
     return Math.floor((Math.random() * 900)+ 100);
@@ -27,3 +29,44 @@ while(i<4)
 }
 //printing value
 console.log(`Max = ${max} \nMin = ${min}`);
+*/
+//----------------------------------------------UC 2 -------------------------------------------------------
+function checkMonth()
+{
+    var day = parseInt( prompt('Enter the day?'));
+    var month = parseInt(prompt('Enter the month in number?'));
+        switch(month)
+        {
+            case 3:
+                if(day>=20 && day<=31)
+                {
+                    return true;
+                }
+                return false;
+            case 4:
+                if(day>=1 && day<=30)
+                {
+                    return true;
+                }
+                return false;
+            case 5:
+                if(day>=1 && day<=31)
+                {
+                    return true;
+                }
+                return false;
+            case 6:
+                if(day>=1 && day<=20)
+                {
+                    return true;
+                }
+                return false;
+            default:
+                return false;
+
+        }
+}
+
+console.log(checkMonth()?"Valid":"Invalid");
+
+
