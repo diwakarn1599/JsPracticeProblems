46c05f702ff7f00e6d5ed4aa153ed41ea9c35e26
Diwakar <diwakar.n@tvsnext.io>
2021-08-13 16:37:56 +0530

    [Diwakar]Add: print the day based on given number

--- JsPracticeProblems/SwitchCaseProblems.js
+++ JsPracticeProblems/SwitchCaseProblems.js
@@ -1,5 +1,7 @@
 //----------------------------------UC1--------------------------------------------
+
 const prompt = require('prompt-sync')();
+/*
 const num = parseInt(prompt('Enter the number?'));
 switch(num)
 {
@@ -35,5 +37,36 @@ switch(num)
         break;
     default:
         console.log("Enter the correct number");
+        break;
+}
+*/
+//-------------------------------------------------UC2--------------------------------
+const num = parseInt(prompt('Enter the number of day in week?'));
+
+switch(num)
+{
+    case 1:
+        console.log("Sunday");
+        break;
+    case 2:
+        console.log("Monday");
+        break;
+    case 3:
+        console.log("Tuesday");
+        break;
+    case 4:
+        console.log("Wednesday");
+        break;
+    case 5:
+        console.log("Thursday");
+        break;
+    case 6:
+        console.log("Friday");
+        break;
+    case 7:
+        console.log("Saturday");
+        break;
+    default:
+        console.log("Enter the day");
         break;
 }
